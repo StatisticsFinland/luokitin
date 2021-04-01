06f8441ecb6ea7b7c69a5075b162862a4fcb08ee
Ilari Laitinen <ilari.laitinen@stat.fi>
2021-04-01 15:15:24 +0300

    text changes in english

--- src/stat-buttons.js
+++ src/stat-buttons.js
@@ -3,56 +3,55 @@ import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-styles/paper-styles.js';
 
 class StatButtons extends PolymerElement {
-
     static get properties() {
         return {
             classificationName: String,
             language: {
                 type: String,
-                value: "fi",
+                value: 'fi',
                 notify: true,
                 reflectToAttribute: true,
             },
             buttonContent: {
                 type: Array,
                 notify: true,
-                value: {first: "Rakennusluokitus 2018", second: "Rakennusluokitusavain", third: "Toimialaluokitusavain"}
+                value: {first: 'Rakennusluokitus 2018', second: 'Rakennusluokitusavain', third: 'Toimialaluokitusavain'},
             },
-        }
+        };
     }
 
     connectedCallback() {
-        super.connectedCallback()
-        this.setLanguage()
+        super.connectedCallback();
+        this.setLanguage();
     }
 
     setLanguage() {
-        if (this.language === "en") {
-            this.buttonContent.first = "Classification of Buildings 2018"
-            this.buttonContent.second = "Building classification conversion key"
-            this.buttonContent.third = "Industrial classification conversion key"
-        } else if (this.language === "sv") {
-            this.buttonContent.first = "Byggnadsklassificering 2018"
-            this.buttonContent.second = "Byggnadsklassificeringsnyckel"
-            this.buttonContent.third = "Näringsgrensindelningsnyckel"
+        if (this.language === 'en') {
+            this.buttonContent.first = 'Classification of Buildings 2018';
+            this.buttonContent.second = 'Building Classification Correspondence Table';
+            this.buttonContent.third = 'Industrial Classification Correspondence Table';
+        } else if (this.language === 'sv') {
+            this.buttonContent.first = 'Byggnadsklassificering 2018';
+            this.buttonContent.second = 'Byggnadsklassificeringsnyckel';
+            this.buttonContent.third = 'Näringsgrensindelningsnyckel';
         }
-        this.notifyPath('buttonContent.first')
-        this.notifyPath('buttonContent.second')
-        this.notifyPath('buttonContent.third')
+        this.notifyPath('buttonContent.first');
+        this.notifyPath('buttonContent.second');
+        this.notifyPath('buttonContent.third');
     }
 
     click(e) {
-        let correspondenceClasses = false   // A variable to be attached in the event details to indicate whether correspondence tables need to be loaded or not.
-        if (e.target.id != "rakennus_1_20180712") {
+        let correspondenceClasses = false; // A variable to be attached in the event details to indicate whether correspondence tables need to be loaded or not.
+        if (e.target.id != 'rakennus_1_20180712') {
             correspondenceClasses = true;
         }
         window.dispatchEvent(new CustomEvent('stat-classification', {
-            detail: {classificationId: e.target.id, correspondenceClasses: correspondenceClasses}
-        }))
-        let buttons = this.shadowRoot.querySelectorAll("paper-button")
+            detail: {classificationId: e.target.id, correspondenceClasses: correspondenceClasses},
+        }));
+        const buttons = this.shadowRoot.querySelectorAll('paper-button');
         buttons.forEach(button => {
             if (button != e.target) {
-                this.removeAttribute(button, "active")
+                this.removeAttribute(button, 'active');
             }
         });
     }
@@ -60,7 +59,7 @@ class StatButtons extends PolymerElement {
     // Might not work on IE and polyfills might not catch it.
     removeAttribute(obj, attr) {
         if (obj.hasAttribute(attr)) {
-            obj.removeAttribute(attr)
+            obj.removeAttribute(attr);
         }
     }
 
